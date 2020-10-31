29a903e4d53b2cc7c52574caf19371ac1f28f40b
vallka <vallka@vallka.com>
2020-10-31 18:02:45 +0000

    look

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from .views import *
 app_name = 'blog'
 
 urlpatterns = [
-    path('', ListView.as_view(), name='blog'),
+    path('', ListView.as_view(), name='list'),
     path('<str:slug>/', PostView.as_view(), name='post'),
 ]
