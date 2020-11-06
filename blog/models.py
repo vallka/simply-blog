7ca52d4315945ec220241225fec602a84325ef4d
vallka <vallka@vallka.com>
2020-11-06 10:42:48 +0000

    ..

--- blog/models.py
+++ blog/models.py
@@ -39,9 +39,9 @@ class Post(models.Model):
     blog = models.BooleanField(_("Publish to blog"),default=False)
     blog_start_dt = models.DateTimeField(_("Publish Date/Time"), blank=True, null=True)
     email = models.BooleanField(_("Send as newsletter"),default=False)
-    email_end_dt = models.DateTimeField(_("Send Date/Time"), blank=True, null=True)
+    email_send_dt = models.DateTimeField(_("Send Date/Time"), blank=True, null=True)
 
-    category = models.ManyToManyField(Category, blank=True, null=True)
+    category = models.ManyToManyField(Category, )
 
     text = MarkdownxField(_("Text"), blank=True, null=False)
 
