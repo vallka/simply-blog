c41d5bac3297be2e90792ec4c5302e7df29cef0f
vallka <vallka@vallka.com>
2020-11-10 22:12:38 +0000

    ..

--- simplyblog/urls.py
+++ simplyblog/urls.py
@@ -21,8 +21,8 @@ from django.conf import settings
 from django.conf.urls.static import static 
 
 urlpatterns = [
+    path('blog/newsletter/', include('newsletter.urls')),
     path('blog/', include('blog.urls')),
-    path('blog/', include('newsletter.urls')),
     path('admin/', admin.site.urls),
     path('markdownx/', include('markdownx.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
