661e4f04ee78d6a90025de1cd6350cb3819e0744
vallka <vallka@vallka.com>
2020-11-11 14:22:29 +0000

    ..

--- simplyblog/settings.py
+++ simplyblog/settings.py
@@ -168,8 +168,8 @@ MARKDOWNX_MARKDOWN_EXTENSIONS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = os.environ['EMAIL_HOST']
-EMAIL_PORT = 587 #os.environ['EMAIL_PORT'] #587
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER'],
+EMAIL_PORT = os.environ['EMAIL_PORT'] #587
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
