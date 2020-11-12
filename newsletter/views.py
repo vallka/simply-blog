4622bb4c5dd6c1dcc7138ecba7da0acdef23f0a5
vallka <vallka@vallka.com>
2020-11-12 23:52:47 +0000

    stats

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import redirect
+from django.utils import timezone
 
 import logging
 logger = logging.getLogger(__name__)
 
+from .models import *
 
 # Create your views here.
 
@@ -10,6 +12,14 @@ def my_image(request):
     uuid=request.GET['uuid']
 
     logger.error("my_image - uuid:%s",uuid)
+    try:
+        ns = NewsShot.objects.get(uuid=uuid)
+        if ns.opened_dt!=None:
+            ns.opened_dt = timezone.now()
+            ns.save()
+    except NewsShot.DoesNotExist:
+        logger.error("NewsShot.DoesNotExist:%s",uuid)
+
 
     return redirect('/static/images/pixel.png')
 
@@ -17,5 +27,16 @@ def click_redirect(request,uuid):
     path=request.GET['path']
     print ('click_redirect',uuid,path)
     logger.error("click_redirect - uuid:%s, path:%s",uuid,path)
+    try:
+        ns = NewsShot.objects.get(uuid=uuid)
+        if ns.clicked_dt!=None:
+            ns.clicked_dt = timezone.now()
+            ns.clicked_qnt = 1
+        else:    
+            ns.clicked_qnt += 1
+        ns.save()
+        
+    except NewsShot.DoesNotExist:
+        logger.error("NewsShot.DoesNotExist:%s",uuid)
 
     return redirect('/' + path)
