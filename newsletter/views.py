5d2f2346f1368d454340967bf90bb0abf008638c
vallka <vallka@vallka.com>
2020-11-22 20:55:17 +0000

    ..

--- newsletter/views.py
+++ newsletter/views.py
@@ -84,10 +84,10 @@ def sendtest(request,slug):
     #if attachment_file: email.attach_file(attachment_file)
     
     send_result = email.send()
-    #message_id = email.extra_headers.get('messageId',None)
-    print('send_result',send_result)
-    logger.error(email.headers)
-    logger.error("send_result:%s",send_result)
+    message_id = email.extra_headers.get('Message-ID','-')
+    print('send_result',send_result,message_id)
+    logger.info(email.extra_headers)
+    logger.error("send_result:%s:%s",send_result,message_id)
 
 
     return HttpResponse({'result':'ok'})    
