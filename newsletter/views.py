e14a74e9678b3293e96b8a78fd757df3f882ebd2
vallka <vallka@vallka.com>
2020-12-03 22:35:54 +0000

    ..

--- newsletter/views.py
+++ newsletter/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import redirect
 from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.conf import settings
 
 import logging
@@ -102,7 +102,7 @@ def sendtest(request,slug):
 
     html = NewsShot.add_html(post.formatted_markdown,post.title,post.slug)
 
-    email = EmailMultiAlternatives( '[TEST NEWSLATTER]' + post.title, post.title, settings.EMAIL_FROM_USER, [to_email], headers = {'X-gel-id': f'xxx-{to_email}-xxx'}  )
+    email = EmailMultiAlternatives( '[TEST NEWSLETTER] ' + post.title, post.title, settings.EMAIL_FROM_USER, [to_email], headers = {'X-gel-id': f'xxx-{to_email}-xxx'}  )
     email.attach_alternative(html, "text/html") 
     #if attachment_file: email.attach_file(attachment_file)
     
@@ -113,4 +113,4 @@ def sendtest(request,slug):
     logger.error("send_result:%s:%s",send_result,message_id)
 
 
-    return HttpResponse({'result':'ok'})    
+    return JsonResponse({'result':'ok'})    
