f98484f7afec4664b1b97a0bdaf0fde6bcf42ad0
vallka <vallka@vallka.com>
2021-02-18 15:47:13 +0000

    home

--- simplyblog/urls.py
+++ simplyblog/urls.py
@@ -19,8 +19,10 @@ from django.urls import include, path, re_path
 from django.conf import settings
 
 from django.conf.urls.static import static 
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('blog/')),
     path('blog/admin/', admin.site.urls),
     path('blog/newsletter/', include('newsletter.urls')),
     path('blog/', include('blog.urls')),
