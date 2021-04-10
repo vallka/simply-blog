087956c6d07d3bbd44caf506d434b9d69cf1355e
vallka <vallka@vallka.com.com>
2021-04-10 21:29:02 +0100

    ..

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .models import *
 
 class ListView(generic.ListView):
     model = Post
-    paginate_by = 10
+    paginate_by = 5
     
     def get_queryset(self):
         return Post.objects.filter(
