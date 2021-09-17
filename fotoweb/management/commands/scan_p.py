207ab99a635ff635ee16171f66f75a7db1a2a3a1
vallka <vallka@vallka.com.com>
2021-09-17 21:23:09 +0100

    o_binary

--- fotoweb/management/commands/scan_p.py
+++ fotoweb/management/commands/scan_p.py
@@ -44,7 +44,8 @@ def open_dir(dir):
                 print (res)
 
                 if not res['response'] or parser.parse(res['response'][0]['createdAt'])<fdt:
-                    fd = pc.file_open(path=f['path'],flags=os.O_BINARY)
+                    #fd = pc.file_open(path=f['path'],flags=os.O_BINARY)
+                    fd = pc.file_open(path=f['path'])
                     data = pc.file_read(fd=fd['fd'],count=1024*1024*100)
                     print ('len=',len(data))
                     upload = imagekit.upload(
