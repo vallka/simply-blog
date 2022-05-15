000cfd91150c6d8995babcdc438c3bc3c9af6589
vallka <vallka@vallka.com.com>
2022-05-15 19:26:43 +0100

    ...

--- castles/admin.py
+++ castles/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Castle
 
-admin.site.register(Castle)
+#admin.site.register(Castle)
+
+@admin.register(Castle)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','name','grid_reference',]
+    search_fields = ['name','description_short', 'closest_to']
